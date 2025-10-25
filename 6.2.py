ef2918770aaa2c8a4c1b0e9af382d564c3ac3eeb
Tomasz Chowaniec <245460@student.uek.krakow.pl>
2025-10-25 12:55:02 +0200

    druga zmiana

---
+++ 6.2.py
@@ -0,0 +1,6 @@
+###
+# A program that prints your initials
+#
+name = 'Tomasz'
+surname = 'Chowaniec'
+print(f'your initials {name[0]}{surname[0]}') 
---
+++ 6.3.py
@@ -0,0 +1,2 @@
+university = "Krakow University of Economics"
+print(university[-9:-1])
